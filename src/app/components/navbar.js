8c184f6c47a0a685d7488f56ae71b29b43521ea2
Gautham <korrapatigautham@gmail.com>
2026-01-08 08:58:56 -0800

    remove blog for now (update after submission

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -32,7 +32,7 @@ const Navbar = () => {
                 {/* <li className="font-inter cursor-pointer hover:text-[#E37633] hover:duration-300" onClick={() => router.push("/vision")}>Vision</li> */}
                 {/* <li className="font-inter cursor-pointer hover:text-[#E37633] hover:duration-300" onClick={() => router.push("/pricing")}>Pricing</li> */}
                 <li className="font-inter cursor-pointer hover:text-[#E37633] hover:duration-300" onClick={() => router.push("/team")}>Team</li>
-                <li className="font-inter cursor-pointer hover:text-[#E37633] hover:duration-300" onClick={() => router.push("/blog")}>Blog</li>
+                {/* <li className="font-inter cursor-pointer hover:text-[#E37633] hover:duration-300" onClick={() => router.push("/blog")}>Blog</li> */}
                 {/* <li className="font-inter cursor-pointer hover:text-[#E37633] hover:duration-300" onClick={() => router.push("/contact")}>Contact</li> */}
             </ul>
 
